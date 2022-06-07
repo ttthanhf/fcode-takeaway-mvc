1b6b00e33058d2b3d90a328b4704e141a30b0e1a
ThanhF <trthanh179@gmail.com>
2022-06-07 14:42:39 +0700

    fix wrong sql script

--- models/createTable.js
+++ models/createTable.js
@@ -1,13 +1,14 @@
 const db = require('../configs/mysql.js');
 
-function isTableExist() {
+function createTableIfItNotExist() {
     db.query('SHOW TABLES LIKE ?', ['account'], function(_,result,err) {
         if(result.length > 0) {
             console.log('Table is exist');
         }
         else {
-            db.query('CREATE TABLE ? (? INT NOT NULL AUTO_INCREMENT,? VARCHAR(45) NOT NULL,? VARCHAR(45) NOT NULL,PRIMARY KEY (?),UNIQUE INDEX ? (? ASC) VISIBLE,UNIQUE INDEX ? (? ASC) VISIBLE)',['account','id','username','password','id','id_UNIQUE', 'id','username_UNIQUE','username']);
+            db.query('CREATE TABLE `account` (`id` INT NOT NULL AUTO_INCREMENT,`username` VARCHAR(45) NOT NULL,`password` VARCHAR(45) NOT NULL,PRIMARY KEY (`id`),UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE,UNIQUE INDEX `username_UNIQUE` (`username` ASC) VISIBLE);')
         }
     });
 }
-module.exports = isTableExist;
+
+module.exports = createTableIfItNotExist;
