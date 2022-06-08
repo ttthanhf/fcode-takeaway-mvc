e194da0e2acf170ff37b5b6ad18a183dfce454fa
ThanhF <trthanh179@gmail.com>
2022-06-08 14:56:05 +0700

    add comment

--- routes/route.js
+++ routes/route.js
@@ -2,13 +2,16 @@ const loginController = require("../controllers/login.controller.js");
 const registerController = require("../controllers/register.controller.js");
 
 function route(app) {
+    //index page
     app.get('/', function(req,res) {
         res.render('index');
     });
     
+    //login page
     app.get('/login', loginController.get);
     app.post('/login', loginController.post);
     
+    //register page
     app.get('/register', registerController.get);
     app.post('/register', registerController.post);
 }
--- server.js
+++ server.js
@@ -1,25 +1,33 @@
+
+//express config
 const express = require('express');
 const app = express();
 
 const path = require('path');
 
+//static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
+//bodyParser config
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
+//handlebar config
 const { engine } = require('express-handlebars');
 app.engine('hbs', engine());
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, './views'));
 
+//router config
 const route = require('./routes/route.js');
 route(app);
 
+//server run at port 3000
 app.listen(3000, () => {
     console.log(`listening at port 3000`);
 });
 
+//create table in mysql if it not exist
 const createTable = require('./models/createTable.js');
 createTable();
